892634e57d89371aa7f660a84e9f36123f0b5750
Harry <victor.nicacio@progmedia.com.br>
2019-04-14 21:15:12 -0300

    Bug Fixes: NestedSerializers

--- playlist/serializers.py
+++ playlist/serializers.py
@@ -15,18 +15,36 @@ class Genre_Serializer(serializers.ModelSerializer):
 
 class Band_Serializer(serializers.ModelSerializer):
 
+    genre = Genre_Serializer(many=False)
+    record = Record_Serializer(many=False)
+
     class Meta:
         model = Band
-        fields = '__all__'
+        fields = (
+            'name',
+            'genre',
+            'record'
+        )
 
 class Music_Serializer(serializers.ModelSerializer):
 
+    band = Band_Serializer(many=False)
+
     class Meta:
         model = Music
-        fields = '__all__'
+        fields = (
+            'name',
+            'band',
+            'duration',
+            'year'
+        )
 
 class Playlist_Serializer(serializers.ModelSerializer):
 
+    music = Music_Serializer(many=True)
     class Meta:
         model = Playlist
-        fields = '__all__'
+        fields = (
+            'name',
+            'music'
+        )
